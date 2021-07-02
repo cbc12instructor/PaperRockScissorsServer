c98864ab0e051bee5e2d343388d0585aefb86dfb
Eric Harrison <github@hrrsn.co>
2021-07-01 20:29:34 -0400

    Show off "simple routing" in a comment.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app     = express();
 
+const PaperRockScissors = require('paperrockscissorsgame');
+let game = new PaperRockScissors();
+
 // game routes contains all game logic for /paper/rock/scissors 
 const gameRoutes = require('./routes/game.js');
-
 app.use('/game', gameRoutes);
+
+// app.get('/game/paper', function(req, res) {
+// 	res.json(game.game('paper'));
+// });	
+// app.get('/game/rock', function(req, res) {
+// 	res.json(game.game('rock'));
+// });	
+// app.get('/game/scissors', function(req, res) {
+// 	res.json(game.game('scissors'));
+// });	
 
 app.use( express.static('html') );
 
--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const PaperRockScissors = require('PaperRockScissorsGame');
+
 const options = ['paper', 'rock', 'scissors'];
 
 // return JSON object = { 'result': 'won/lost/tie', player: 'paper', 'computer': 'rock' }
@@ -14,35 +16,11 @@ router.get('/scissors', paperRockScissorsChoice);
 function paperRockScissorsChoice(req, res) {
 	console.log(req.url);
 	let playerChoice = req.url.split('/')[1];
-	let computerChoice = options[Math.floor(Math.random() * options.length)];
 
-	let results = {
-		'player': playerChoice,
-		'computer': computerChoice,
-		'result': 'tie'
-	};
+	let game = new PaperRockScissors();
+	
+	res.json( game.game(playerChoice) );
 
-	if ( playerChoice == 'paper' ) {
-		if ( computerChoice == 'rock' ) {
-			results.result = 'win';
-		} else if ( computerChoice == 'scissors' ) {
-			results.result = 'loss';
-		}
-	} else if ( playerChoice == 'rock' ) {
-		if ( computerChoice == 'scissors' ) {
-			results.result = 'win';
-		} else if ( computerChoice == 'paper' ) {
-			results.result = 'loss';
-		}
-	} else if ( playerChoice == 'scissors' ) {
-		if ( computerChoice == 'paper' ) {
-			results.result = 'win';
-		} else if ( computerChoice == 'rock' ) {
-			results.result = 'loss';
-		}
-	}
-
-	res.json(results);
 }
 
 module.exports = router;
